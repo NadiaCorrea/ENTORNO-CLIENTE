55bf751300ec5b55756a0ef7c2a63de7c0b16ae6
NadiaCorrea <ncormor536@gmail.com>
2022-09-23 20:16:28 +0200

    ejercicios4 and 5 done

--- ejerciciosSelectoresDOM/Ejercicio4.js
+++ ejerciciosSelectoresDOM/Ejercicio4.js
@@ -1,10 +1,33 @@
-/*
- A partir del fichero Ejercicio4.html se pide realizar mediante javascritp las siguientes 
- cuestiones utilizando selectores CSS.
-    1 - Selecciona todos los elementos de tipo h2 y pon el texto de color rojo.
-    2 - Modifica el precio del vuelo para que ahora sea $199.99
-    3 - Modifica todos los títulos de las vacaciones (h2), su nuevo valor será "Sanlúcar de Barrameda".
-    4 - Selecciona la lista de vacaciones cuyo id es #vacations.
-    5 - Selecciona aquellos ítems que tenga la clase .america
-    */
+/*A partir del fichero Ejercicio4.html se pide realizar mediante javascritp las siguientes 
+ cuestiones utilizando selectores CSS.    */
 
+//1 - Selecciona todos los elementos de tipo h2 y pon el texto de color rojo.
+
+let headings = document.querySelectorAll("h2");
+for(let i = 0; i < headings.length; i ++){
+   headings[i].style.color="red";
+}
+
+//2 - Modifica el precio del vuelo para que ahora sea $199.99
+
+let price = document.querySelectorAll(".details");
+for (let i= 0; i < price.length; i ++){
+   price[i].innerText = "$199.99";
+}
+
+//3 - Modifica todos los títulos de las vacaciones (h2), su nuevo valor será "Sanlúcar de Barrameda".
+
+for (i=0; i < headings.length; i ++){
+   headings[i].innerText = "Sanlúcar de Barrameda";
+
+}
+
+// 4 - Selecciona la lista de vacaciones cuyo id es #vacations.
+
+let list = document.querySelectorAll("#vacations");
+console.log(list);
+
+//5 - Selecciona aquellos ítems que tenga la clase .america
+
+let items = document.querySelectorAll(".america");
+console.log(items);
--- ejerciciosSelectoresDOM/Ejercicio5.js
+++ ejerciciosSelectoresDOM/Ejercicio5.js
@@ -1,17 +1,75 @@
 /*
 A partir del fichero Ejercicio5.html se pide realizar mediante js 
 las siguientes cuestiones utilizando selectores CSS, 
-una vez seleccionados modifica alguna propiedad CSS.
-    a - Selecciona todos los módulos de primero
-    b - Selecciona el módulo Sistemas Informáticos
-    c - Selecciona el módulo que va detrás de Sistemas Informáticos
-    d - Selecciona los módulos que van detrás de Sistemas Informáticos
-    e - Selecciona los módulos que tengan el atributo href
-    f - Selecciona los módulos que no tengan el atributo href
-    g - Selecciona aquellos módulos que contengan la cadena 'mod'
-    h - Selecciona aquellos módulos que empiecen por la cadena 'mod'
-    i - Selecciona aquellos módulos que terminen con la cadena '.html'
-    j - Selecciona todas las horas de los módulos de segundo
-    k - Selecciona los ítems vacíos y añade el contenido 'Nodo vacío'
-    h - Selecciona los módulos DWESE, DAW y EIE
-    */
+una vez seleccionados modifica alguna propiedad CSS.    
+*/
+
+//a - Selecciona todos los módulos de primero
+let modulosPri = document.querySelector( "p + ul");
+console.log(modulosPri);
+
+//b - Selecciona el módulo Sistemas Informáticos
+let sistemas = document.querySelector("[href= 'SImod.html']");
+console.log(sistemas);
+
+//c - Selecciona el módulo que va detrás de Sistemas Informáticos
+let nextModulo = document.querySelector("ul li:nth-of-type(4)");
+//let nextModulo1 = modulosPri.querySelector("li:nth-of-type(4)");
+console.log(nextModulo);
+
+//d - Selecciona los módulos que van detrás de Sistemas Informáticos
+let modulos = modulosPri.querySelectorAll("li:nth-of-type(1n+4)");
+console.log(modulos);
+
+//e - Selecciona los módulos que tengan el atributo href
+let modHref = document.querySelectorAll("[href]");
+console.log(modHref);
+
+//f - Selecciona los módulos que no tengan el atributo href
+let sinHref = document.querySelectorAll("a:not([href])");
+console.log(sinHref);
+
+//g - Selecciona aquellos módulos que contengan la cadena 'mod'
+let cadMod = document.querySelectorAll('[href*= "mod"]');
+console.log(cadMod);
+
+//h - Selecciona aquellos módulos que empiecen por la cadena 'mod'
+let startMod = document.querySelectorAll('[href^= "mod"]');
+console.log(startMod);
+
+//i - Selecciona aquellos módulos que terminen con la cadena '.html'
+let endHtml = document.querySelectorAll("[href$='.html']");
+console.log(endHtml);
+
+//j - Selecciona todas las horas de los módulos de segundo
+let horas = document.querySelectorAll("li>ul li:last-child") 
+console.log(horas);
+
+//k - Selecciona los ítems vacíos y añade el contenido 'Nodo vacío'
+let emptyItems = document.querySelectorAll("li:empty");
+for(let i=0; i < emptyItems.length; i ++){
+    emptyItems[i].textContent = "Nodo vacío";
+}
+console.log(emptyItems);
+
+//l - Selecciona los módulos DWESE, DAW y EIE
+
+let modName = document.querySelectorAll("ul>li>ul");
+let parents = [];
+let requestedMod = [];
+
+for(let i=0; i < modName.length; i++){
+    parents.push(modName[i].parentElement);
+}
+for(let j=0; j < parents.length ; j = j +2){
+    requestedMod.push(parents[j].firstChild);    
+}
+
+console.log(modName);
+console.log(parents);
+console.log(requestedMod);
+
+//ul>li>ul   ul>li
+
+
+
